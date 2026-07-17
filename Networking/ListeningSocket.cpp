a22bb651a30cba75719cc1fa3b41c20d6464e798
Garvit Singla <garvits093@gmail.com>
2026-07-17 19:39:24 +0530

    Added listening socket

---
+++ Networking/ListeningSocket.cpp
@@ -0,0 +1,13 @@
+#include "ListeningSocket.hpp"
+#include "BindingSocket.hpp"
+#include <sys/socket.h>
+
+
+HDE::ListeningSocket::ListeningSocket(int domain,int service,int protocol,int port,u_long interface,int bklg): BindingSocket(domain,service,protocol,port,interface){
+    backlog = bklg;
+    start_listening();
+    test_connection(listening);
+};
+void HDE::ListeningSocket::start_listening(){
+    listening = listen(get_sock(), backlog);
+}
---
+++ Networking/ListeningSocket.hpp
@@ -0,0 +1,19 @@
+#ifndef ListeningSocket_hpp
+#define ListeningSocket_hpp
+
+
+#include "BindingSocket.hpp"
+
+namespace HDE {
+    class ListeningSocket :public BindingSocket{
+        private:
+            int backlog;
+            int listening;
+        public:
+            ListeningSocket(int domain,int service,int protocol,int port,u_long interface,int bklg);
+            void start_listening();
+
+    };
+}
+
+#endif
